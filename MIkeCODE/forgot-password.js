706b598c49d4e5d8444dd69fbfd1d854316d6b31
MikeBrocode <mikeadam192@gmail.com>
2025-03-24 08:58:45 +0800

    Delete MIkeCODE directory

--- MIkeCODE/forgot-password.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('forgotPasswordForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    const email = document.getElementById('resetEmail').value;
-    
-    // In a real application, this would send a reset link to the user's email
-    alert('If an account exists with this email, you will receive password reset instructions.');
-    window.location.href = 'login.html';
-});
